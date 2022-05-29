a8c0f7543d2ff7d9c522b38c750fcec1ae20391c
Bhagya Mudgal <bhagyamudgal@gmail.com>
2022-05-29 18:02:50 +0530

    completed contract

---
+++ constants/index.js
@@ -0,0 +1,7 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x586062F440A29d6eCB4aC22d758C9FC58a8403D9";
+
+// URL to extract Metadata for a Chimp NFT
+const METADATA_URL = "https://chimps-academy-nft-collection.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
